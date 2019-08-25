52df59080b86ee0df99f327116dea84bccd68eb2
Reed James <reedtjames1234@gmail.com>
2019-08-24 19:00:42 -0500

    Users-router.js created, untested

--- users/users-router.js
+++ users/users-router.js
@@ -1,33 +1,14 @@
-const db = require("../data/db-config.js");
+const router = require("express").Router();
 
-module.exports = {
-  add,
-  find,
-  findBy,
-  findById
-};
+const users = require("./users-model.js");
+const restricted = require("../auth/restricted-middleware.js");
 
-function find() {
-  return db("users").select("id", "username");
-}
-
-function findBy(filter) {
-  return db("users")
-    .where(filter)
-    .first();
-}
-
-function findById(id) {
-  return db("users")
-    .where({ id })
-    .first();
-}
-
-function add(user) {
-  return db("users")
-    .insert(user, "id")
-    .then(ids => {
-      const [id] = ids;
-      return findById(id);
-    });
-}
+// router.get('/', restricted, (req, res) => {
+router.get("/", (req, res) => {
+  users
+    .find()
+    .then(user => {
+      res.json(user);
+    })
+    .catch(err => res.send(err));
+});

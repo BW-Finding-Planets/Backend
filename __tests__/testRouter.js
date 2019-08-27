15bf1254afdce7713f16a9b7bafb0b1e2e24da94
Reed James <reedtjames1234@gmail.com>
2019-08-27 13:32:27 -0500

    Router testing, register 2 passed, login 1 passed

--- __tests__/testRouter.js
+++ __tests__/testRouter.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-
+const bcrypt = require("bcryptjs");
 const db = require("../data/db-config.js");
 const server = require("../server.js");
 
@@ -10,7 +10,7 @@ describe("server", () => {
   it('Tests are tunning with DB_ENV set as "testing"', () => {
     expect(process.env.DB_ENV).toBe("testing");
   });
-  describe("post /auth/register", () => {
+  describe("POST /auth/register", () => {
     it("Should return 201 when a user is added", () => {
       return request(server)
         .post("/auth/register")
@@ -25,6 +25,31 @@ describe("server", () => {
     it("should return JSON to register", () => {
       return request(server)
         .post("/auth/register")
+        .send({
+          username: "test",
+          password: "pass"
+        })
+        .then(res => {
+          expect(res.type).toMatch(/json/);
+        });
+    });
+  });
+  describe("POST /auth/login", () => {
+    it("should return 200 when a user is logging in", () => {
+      return request(server)
+        .post("/auth/login")
+        .send({
+          username: "test",
+          password: bcrypt.hashSync("pass")
+        })
+        .then(res => {
+          expect(res.status).toBe(200);
+        });
+    });
+
+    it("should return JSON to login", () => {
+      return request(server)
+        .post("/auth/login")
         .send({
           username: "test",
           password: "pass"
